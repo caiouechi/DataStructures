9bb942b21658af33fc8bf8f82b20dc98f425bc2d
Caio Uechi <caio.uechi@soti.net>
2020-02-27 12:19:42 -0500

    HeightTreeBalanced

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace HeightBalancedBinaryTree
 {
     class Program
     {
+        public static bool isBalanced = true;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Data structures ED!");
+            Node exercise = new Node(1);
+            exercise.left = new Node(2);
+            //exercise.right = new Node(3);
+            exercise.left.left = new Node(4);
+            //exercise.left.left.left = new Node(5);
+
+            Console.WriteLine(CountHeight((exercise)));
+            Console.WriteLine("isBalanced: ");
+            Console.WriteLine(isBalanced);
+            Console.ReadLine();
+        }
+
+
+        public static int CountHeight(Node node)
+        {
+            if (node == null) return 0;
+
+            var heightLeft = 1 + CountHeight(node.left);
+            var heightRight= 1 + CountHeight(node.right);
+
+            var subtraction = heightLeft - heightRight;
+            if (subtraction < 0)
+            {
+                subtraction = (subtraction * -1);
+            }
+
+            if (subtraction > 1)
+            {
+                isBalanced = false;
+            }
+
+            if (heightLeft >= heightRight)
+            {
+                return heightLeft;
+            }
+            else
+            {
+                return heightRight;
+            }
+        }
+    }
+
+    public class Node
+    {
+        public Node left;
+        public Node right;
+        public object data;
+
+        public Node(object data)
+        {
+            this.data = data;
         }
     }
 }
